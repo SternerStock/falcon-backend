23af82656d6707af4692ec166264b1022d46f09f
Corey Laird <lairdcc@gmail.com>
2020-07-05 19:54:04 -0700

    Added check to allow commanders with no printing yet defined.

--- Falcon.API.Core/Controllers/MtGController.cs
+++ Falcon.API.Core/Controllers/MtGController.cs
@@ -41,9 +41,10 @@
 
         [HttpGet("Commanders")]
         public async Task<IEnumerable<CardDto>> GetCommanders(string variant = "Commander", bool allowSilver = false) => await this.context.Legalities
-            .Where(l => l.Format == variant.Replace(" ", string.Empty)
-                     && ((l.LegalAsCommander && !l.Card.Printings.All(p => p.Set.SetType.Name == "funny"))
-                      || (l.LegalAsCommander && allowSilver && l.Card.Printings.All(p => p.Set.SetType.Name == "funny"))))
+            .Where(l => l.Format == variant.Replace(" ", string.Empty) && l.LegalAsCommander
+                     && (!l.Card.Printings.Any()
+                      || !l.Card.Printings.All(p => p.Set.SetType.Name == "funny")
+                      || allowSilver && l.Card.Printings.All(p => p.Set.SetType.Name == "funny")))
             .IncludeCardProperties()
             .OrderBy(l => l.Card.Name)
             .Select(l => new CardDto(l.Card))
@@ -59,9 +60,10 @@
             }
 
             var legalities = this.context.Legalities
-                .Where(l => l.Format == variant.Replace(" ", string.Empty) && l.CardID != cmdrId
-                         && ((l.LegalAsCommander && !l.Card.Printings.All(p => p.Set.SetType.Name == "funny"))
-                          || (l.LegalAsCommander && allowSilver && l.Card.Printings.All(p => p.Set.SetType.Name == "funny"))));
+                .Where(l => l.Format == variant.Replace(" ", string.Empty) && l.CardID != cmdrId && l.LegalAsCommander
+                         && (!l.Card.Printings.Any()
+                      || !l.Card.Printings.All(p => p.Set.SetType.Name == "funny")
+                      || allowSilver && l.Card.Printings.All(p => p.Set.SetType.Name == "funny")));
 
             if (cmdr.OracleText.Contains("Partner with"))
             {
